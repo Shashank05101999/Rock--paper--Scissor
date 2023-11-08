56f7aecfa2bdc0196e99492ab47797819fd281ea
Shashank05101999 <143600573+Shashank05101999@users.noreply.github.com>
2023-11-08 21:16:59 +0530

    Add files via upload

---
+++ Rockpaperscissorsgame.java
@@ -0,0 +1,36 @@
+
+package rockpaperscissorsgame;
+
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class Rockpaperscissorsgame {
+
+    
+    public static void main(String[] args) {
+      
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter 0 for Rock, 1 for paper , 2 for scissor");
+        int userInput=sc.nextInt();
+        
+        Random random =new Random();
+        
+        int computerInput = random.nextInt(3);
+        
+        if(userInput ==computerInput)
+        {
+            System.out.println("Draw");
+            
+        }
+        else if(userInput==0 && computerInput==2 || userInput==1 && computerInput==0 || userInput==2 && computerInput==1)
+        {
+            System.out.println(" You win ");
+        }else {
+         
+            System.out.println("computer Win !");
+        }
+        System.out.println("computer choice:"+computerInput);
+    }
+    
+}
